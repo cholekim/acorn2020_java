7a7757590b9e6b3d842d3a4a26e60fadb371fd1d
kimnahyeon <nahyeon927@gmail.com>
2021-05-04 14:43:11 +0900

    Step04 MyMain 추가함

---
+++ Step04_Class/src/test/study/MyMain01.java
@@ -0,0 +1,11 @@
+package test.study;
+
+import test.mypac.Car;
+
+public class MyMain01 {
+	public static void main(String[] args) {
+		System.out.println();
+		Car c1=new Car();
+		Car c2=new Car();
+	}
+}
