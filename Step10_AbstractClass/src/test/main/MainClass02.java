9571e79564c927f898cc3bca9e79933b538f88be
kimnahyeon <nahyeon927@gmail.com>
2021-05-07 17:18:52 +0900

    Step09 Sword 추가함

--- Step10_AbstractClass/src/test/main/MainClass02.java
+++ Step10_AbstractClass/src/test/main/MainClass02.java
@@ -1,15 +1,18 @@
 package test.main;
 
 import test.mypac.Gun;
+import test.mypac.Sword;
 import test.mypac.Weapon;
 
 public class MainClass02 {
 	public static void main(String[] args) {
 		//여러분이 직접 클래스를 만들고 객체 생성을 해서 
 		//아래의 useWeapon() 메소드를 호출해 보세요.
-		Weapon w=new Gun();
+		Weapon g=new Gun();
+		MainClass02.useWeapon(g);
 		
-		MainClass02.useWeapon(w);
+		Weapon s=new Sword();
+		MainClass02.useWeapon(s);
 	}
 	
 	public static void useWeapon(Weapon w) {
---
+++ Step10_AbstractClass/src/test/mypac/Sword.java
@@ -0,0 +1,10 @@
+package test.mypac;
+
+public class Sword extends Weapon {
+
+	@Override
+	public void attack() {
+		System.out.println("김구라를 칼로 찔러요");
+	}
+
+}
