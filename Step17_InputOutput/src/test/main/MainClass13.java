6c6fcda1c8142d9e614b78c23938edd260bc9c65
kimnahyeon <nahyeon927@gmail.com>
2021-05-12 17:29:59 +0900

    Step17 내용 추가함

--- Step17_InputOutput/src/test/main/MainClass13.java
+++ Step17_InputOutput/src/test/main/MainClass13.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class MainClass13 {
 	public static void main(String[] args) {
-		File memoFile=new File("c:/acorn2020/myFolder/memo.txt");
+		File memoFile=new File("c:/Users/xogns/Desktop/acorn202104/myFolder/memo.txt");
 		try {
 			if(!memoFile.exists()) {
 				System.out.println("파일이 존재 하지 않습니다.");
@@ -14,6 +14,10 @@ public class MainClass13 {
 			}
 			//파일에서 문자열을 읽어들일 객체
 			FileReader fr=new FileReader(memoFile);
+			/*
+			 * memo.txt 파일에 몇개의 문자가 있는지는 잘 모름
+			 * 아래의 3줄의 코드를 memo.txt 파일에 있는 모든 문자를 읽어올 때까지 반복 수행하고 싶음
+			 */
 			while(true) {
 				//글자 하나 하나의 코드값을 반복문 돌면서 읽어들인다. 
 				int code=fr.read();
--- Step17_InputOutput/src/test/main/MainClass14.java
+++ Step17_InputOutput/src/test/main/MainClass14.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class MainClass14 {
 	public static void main(String[] args) {
-		File memoFile=new File("c:/acorn2020/myFolder/memo.txt");
+		File memoFile=new File("c:/Users/xogns/Desktop/acorn202104/myFolder/memo.txt");
 		try {
 			if(!memoFile.exists()) {
 				System.out.println("파일이 존재 하지 않습니다.");
@@ -17,9 +17,10 @@ public class MainClass14 {
 			FileReader fr=new FileReader(memoFile);
 			BufferedReader br=new BufferedReader(fr);
 			while(true) {
+				//이클립스야 아래의 2줄의 코드를 더이상 읽을 문자열이 없을 때까지 반복해줘~
 				//반복문 돌면서 문자열을 줄단위로(개행기호 기준) 읽어낸다.
 				String line=br.readLine();
-				if(line==null) {//더이상 읽을 문자열이 없으면
+				if(line==null) {//만일 더이상 읽을 문자열이 없으면
 					break;//반복문 탈출
 				}
 				//읽은 문자열 출력하기 
